5cc7bf831d97becbc97416784d4482e9cc70df89
Luke Edwards <luke.edwards05@gmail.com>
2021-06-23 10:41:01 -0700

    chore: fix `factors.js` typo

--- tests/benchmark/factors/factors.js
+++ tests/benchmark/factors/factors.js
@@ -3,7 +3,7 @@
 // which makes more faster than other bytecode interpreted
 // VM language listed here.
 
-var start = process.hrtime()
+var start = process.hrtime();
 
 var N = 50000000;
 var i=0, factors = []
@@ -11,6 +11,6 @@ for (; i <= N; i++) {
 	if (N % i === 0) factors.push(i);
 }
 
-var end = process.hrtime(end);
+var end = process.hrtime(start);
 var secs = (end[0] + end[1] / 1e9).toFixed(6) + 's';
 console.log('elapsed:', secs);
