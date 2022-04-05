370b620066572d2569c7343e937e435719c33d9d
Thakee Nathees <thakeenathees@gmail.com>
2022-04-06 02:39:31 +0530

    deployment after try page re-designed

--- static/js/docs.js
+++ static/js/docs.js
@@ -1,13 +1,4 @@
 
-let $ = document.querySelector.bind(document);
-let $$ = document.querySelectorAll.bind(document);
-
-// Syntax highlighting.
-document.addEventListener('DOMContentLoaded', (event) => {
-  $$('.highlight').forEach(function(el) {
-    hljs.highlightElement(el);
-  });
-});
 
 $("#toggle-menu").onclick = function() {
   $("#navigation").classList.toggle("collapsed");
