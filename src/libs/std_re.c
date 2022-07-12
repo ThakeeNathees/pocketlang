72181fb2c2714d5138729d670517f031e1051f3a
khchen <khchen@gmail.com>
2022-07-12 12:19:47 +0800

    * use a new regex engine based on pike vm   - utf8 and almost all regex pattern are supported   - guarantees that any input regex will scale O(n) with the size of the string * re.replace can accept a closure for string substitute. * complete test suite.

--- src/libs/std_re.c
+++ src/libs/std_re.c
@@ -10,261 +10,261 @@
 #include "../core/vm.h"
 #endif
 
-// https://github.com/cesanta/slre
-#include "thirdparty/slre/slre.h"
-#include "thirdparty/slre/slre.c"
+#include "thirdparty/pikevm/re.c"
 
-typedef struct slre_cap capture;
-
-#define MAX_CAPTURES 256
-#define RE_IGNORECASE SLRE_IGNORE_CASE // 1
+#define RE_IGNORECASE 1
 #define RE_GLOBAL 2
 
-static void _re_error(PKVM* vm, int result) {
-  switch (result) {
-    case SLRE_UNEXPECTED_QUANTIFIER:
-      pkSetRuntimeError(vm, "unexpected quantifier"); break;
-    case SLRE_UNBALANCED_BRACKETS:
-      pkSetRuntimeError(vm, "unbalanced brackets"); break;
-    case SLRE_INTERNAL_ERROR:
-      pkSetRuntimeError(vm, "internal error"); break;
-    case SLRE_INVALID_CHARACTER_SET:
-      pkSetRuntimeError(vm, "invalid character_set"); break;
-    case SLRE_INVALID_METACHARACTER:
-      pkSetRuntimeError(vm, "invalid metacharacter"); break;
-    case SLRE_CAPS_ARRAY_TOO_SMALL:
-      pkSetRuntimeError(vm, "caps array too small"); break;
-    case SLRE_TOO_MANY_BRANCHES:
-      pkSetRuntimeError(vm, "too many branches"); break;
-    case SLRE_TOO_MANY_BRACKETS:
-      pkSetRuntimeError(vm, "too many brackets"); break;
+static RE* _re_init(PKVM* vm, const char** input, uint32_t* len, 
+    int flag_argc, int* global) {
+
+  const char *pattern; uint32_t pattern_len;
+  if (!pkValidateSlotString(vm, 1, &pattern, &pattern_len)) return NULL;
+  if (!pkValidateSlotString(vm, 2, input, len)) return NULL;
+
+  int32_t flags = 0;
+  if (pkGetArgc(vm) >= flag_argc) {
+    if (!pkValidateSlotInteger(vm, flag_argc, &flags)) return NULL;
   }
+
+  if (global) *global = (flags & RE_GLOBAL) == RE_GLOBAL;
+  int insensitive = (flags & RE_IGNORECASE) == RE_IGNORECASE;
+
+  RE* re = re_compile(pattern, insensitive);
+  if (!re) {
+    pkSetRuntimeError(vm, "Cannot compile the regex pattern.");
+    return NULL;
+  }
+
+  return re;
 }
 
-static bool _re_init(PKVM* vm, const char** pattern, const char** input,
-    uint32_t* pattern_len, uint32_t* input_len, capture** caps) {
+static void _re_match(PKVM* vm, RE* re, const char *input, uint32_t len, 
+    bool global, bool range) {
 
-  if (!pkValidateSlotString(vm, 1, pattern, pattern_len)) return false;
-  if (!pkValidateSlotString(vm, 2, input, input_len)) return false;
+  pkNewList(vm, 0);
+  const char *ptr = input;
+  do {
+    const char** matches = re_match(re, ptr);
+    if (!matches) break;
 
-  // let result[0] be entire match
-  if (*pattern_len != 0 && *pattern[0] == '^') {
-    pkSetSlotStringFmt(vm, 1, "^(%.*s)", *pattern_len - 1, *pattern + 1);
-  } else {
-    pkSetSlotStringFmt(vm, 1, "(%.*s)", *pattern_len, *pattern);
-  }
-  if (!pkValidateSlotString(vm, 1, pattern, pattern_len)) return false;
+    for (int i = 0; i < re_max_matches(re); i += 2) {
+      if (matches[i] && matches[i + 1]) {
+        if (range) pkNewRange(vm, 1, matches[i] - input, matches[i + 1] - input);
+        else  pkSetSlotStringLength(vm, 1, matches[i], matches[i + 1] - matches[i]);
+        pkListInsert(vm, 0, -1, 1);
+      } else {
+        if (range) pkSetSlotNull(vm, 1);
+        else pkSetSlotStringLength(vm, 1, ptr, 0);
+        pkListInsert(vm, 0, -1, 1);
+      }
+    }
+    if (ptr == matches[1]) break; // cannot advance
 
-  *caps = (capture*) pkRealloc(vm, NULL, sizeof(capture) * MAX_CAPTURES);
-  ASSERT(*caps != NULL, "pkRealloc failed.");
-  memset(*caps, 0, sizeof(capture) * MAX_CAPTURES);
-  return true;
-}
-
-static void _re_exit(PKVM* vm, capture* caps) {
-  pkRealloc(vm, caps, 0);
+    ptr = matches[1]; // point to last matched char
+  } while (global && ptr < input + len);  
 }
 
 DEF(_reMatch,
   "re.match(pattern:String, input:String[, flag:Number]) -> List",
-  "Perform a regular expression match and return a list of matches. Supported pattern:\n"
-  "  ^       Match beginning of a buffer\n"
-  "  $       Match end of a buffer\n"
-  "  ()      Grouping and substring capturing\n"
-  "  \\s      Match whitespace\n"
-  "  \\S      Match non-whitespace\n"
-  "  \\w      Match alphanumeric [a-zA-Z0-9_]\n"
-  "  \\W      Match non-alphanumeric\n"
-  "  \\d      Match decimal digit\n"
-  "  \\D      Match non-decimal digit\n"
-  "  \\n      Match new line character\n"
-  "  \\r      Match line feed character\n"
-  "  \\f      Match form feed character\n"
-  "  \\v      Match vertical tab character\n"
-  "  \\t      Match horizontal tab character\n"
-  "  \\b      Match backspace character\n"
-  "  +       Match one or more times (greedy)\n"
-  "  +?      Match one or more times (non-greedy)\n"
-  "  *       Match zero or more times (greedy)\n"
-  "  *?      Match zero or more times (non-greedy)\n"
-  "  ?       Match zero or once (greedy)\n"
-  "  x|y     Match x or y (alternation operator)\n"
-  "  \\meta   Match one of the meta character: ^$().[]*+?|\\\n"
-  "  \\xHH    Match byte with hex value 0xHH, e.g. \\x4a\n"
-  "  [...]   Match any character from set. Ranges like [a-z] are supported\n"
-  "  [^...]  Match any character but ones from set\n\n"
+  "Perform a regular expression match and return a list of matches.\n\n"
+  "Supported patterns:\n"
+  "  ^        Match beginning of a buffer\n"
+  "  $        Match end of a buffer\n"
+  "  (...)    Grouping and substring capturing\n"
+  "  (?:...)  Non-capture grouping\n"
+  "  \\s       Match whitespace [ \\t\\n\\r\\f\\v]\n"
+  "  \\S       Match non-whitespace [^ \\t\\n\\r\\f\\v]\n"
+  "  \\w       Match alphanumeric [a-zA-Z0-9_]\n"
+  "  \\W       Match non-alphanumeric [^a-zA-Z0-9_]\n"
+  "  \\d       Match decimal digit [0-9]\n"
+  "  \\D       Match non-decimal digit [^0-9]\n"
+  "  \\n       Match new line character\n"
+  "  \\r       Match line feed character\n"
+  "  \\f       Match form feed character\n"
+  "  \\v       Match vertical tab character\n"
+  "  \\t       Match horizontal tab character\n"
+  "  \\b       Match backspace character\n"
+  "  +        Match one or more times (greedy)\n"
+  "  +?       Match one or more times (non-greedy)\n"
+  "  *        Match zero or more times (greedy)\n"
+  "  *?       Match zero or more times (non-greedy)\n"
+  "  ?        Match zero or once (greedy)\n"
+  "  ??       Match zero or once (non-greedy)\n"
+  "  x|y      Match x or y (alternation operator)\n"
+  "  \\meta    Match one of the meta character: ^$().[]*+?|\\\n"
+  "  \\xHH     Match byte with hex value 0xHH, e.g. \\x4a\n"
+  "  \\<, \\>   Match start-of-word and end-of-word.\n"
+  "  [...]    Match any character from set. Ranges like [a-z] are supported\n"
+  "  [^...]   Match any character but ones from set\n"
+  "  {n}      Matches exactly n times.\n"
+  "  {n,}     Matches the preceding character at least n times.\n"
+  "  {n,m}    Matches the preceding character at least n and at most m times.\n\n"
   "Flags:\n"
   "  re.I, re.IGNORECASE    Perform case-insensitive matching\n"
-  "  re.G, re.GLOBAL        Perform global matching"
+  "  re.G, re.GLOBAL        Perform global matching"  
   ) {
 
   int argc = pkGetArgc(vm);
   if (!pkCheckArgcRange(vm, argc, 2, 3)) return;
 
-  int32_t flags = 0;
-  if (argc == 3) {
-    if (!pkValidateSlotInteger(vm, 3, &flags)) return;
-  }
+  const char *input; uint32_t len; int global;
+  RE* re = _re_init(vm, &input, &len, 3, &global);
+  if (!re) return;
 
-  const char *pattern, *input;
-  uint32_t pattern_len, input_len;
-  capture* caps;
-  if (!_re_init(vm, &pattern, &input, &pattern_len, &input_len, &caps)) return;
+  _re_match(vm, re, input, len, global, false);
+  re_free(re);
+}
 
-  pkReserveSlots(vm, 5);
-  pkNewList(vm, 0);
+DEF(_reRange,
+  "re.range(pattern:String, input:String[, flag: Number]) -> List",
+  "Perform a regular expression match and return a list of range object.\n"
+  "Run help(re.match) to show supported regex patterns."
+  ) {
 
-  int i = 0;
-  while (input_len > 0) {
-    i = slre_match(pattern, input, input_len, caps,
-      MAX_CAPTURES, flags & SLRE_IGNORE_CASE);
+  int argc = pkGetArgc(vm);
+  if (!pkCheckArgcRange(vm, argc, 2, 3)) return;
 
-    if (i < 0 || i > input_len) break;
-    input += i;
-    input_len -= i;
+  const char *input; uint32_t len; int global;
+  RE* re = _re_init(vm, &input, &len, 3, &global);
+  if (!re) return;
 
-    for(int j = 0; j < MAX_CAPTURES && caps[j].ptr != NULL; j++) {
-      pkSetSlotStringLength(vm, 4, caps[j].ptr, caps[j].len);
-      pkListInsert(vm, 0, -1, 4);
-      caps[j].ptr = NULL; // reset the ptr after the string is stored
-    }
-
-    if ((flags & RE_GLOBAL) != RE_GLOBAL) break;
-  }
-  _re_error(vm, i);
-  _re_exit(vm, caps);
+  _re_match(vm, re, input, len, global, true);
+  re_free(re);
 }
 
 DEF(_reTest,
-  "re.test(pattern:String, input:String[, flag:Number]) -> Bool",
-  "Perform a regular expression match and return true of false.\n"
-  "Run help(re.match) for details."
+  "re.test(pattern:String, input:String[, flag: Number]) -> Bool",
+  "Perform a regular expression match and return true or false.\n"
+  "Run help(re.match) to show supported regex patterns."
   ) {
 
   int argc = pkGetArgc(vm);
   if (!pkCheckArgcRange(vm, argc, 2, 3)) return;
 
-  int32_t flags = 0;
-  if (argc == 3) {
-    if (!pkValidateSlotInteger(vm, 3, &flags)) return;
-  }
+  const char *input; uint32_t len;
+  RE* re = _re_init(vm, &input, &len, 3, NULL);
+  if (!re) return;
 
-  const char *pattern, *input;
-  uint32_t pattern_len, input_len;
-  capture* caps;
-  if (!_re_init(vm, &pattern, &input, &pattern_len, &input_len, &caps)) return;
+  const char** matches = re_match(re, input);
 
-  int i = slre_match(pattern, input, input_len, caps,
-    MAX_CAPTURES, flags & SLRE_IGNORE_CASE);
+  pkSetSlotBool(vm, 0, matches != NULL);
 
-  pkSetSlotBool(vm, 0, !(i < 0 || i > input_len));
-
-  _re_error(vm, i);
-  _re_exit(vm, caps);
+  re_free(re);
 }
 
 DEF(_reReplace,
-  "re.replace(pattern:String, input:String, by:String[, count:Number]) -> String",
-  "Perform a regular expression search and replace and return a new string.\n"
-  "Run help(re.match) for details."
+  "re.replace(pattern:String, input:String, [by:String|Closure, count:Number, flag:Number]) -> String",
+  "Replaces [pattern] in [input] by the string [by].\n"
+  "Run help(re.match) to show supported regex patterns."
   ) {
+    
 
   int argc = pkGetArgc(vm);
-  if (!pkCheckArgcRange(vm, argc, 3, 4)) return;
+  if (!pkCheckArgcRange(vm, argc, 2, 5)) return;
+
+  int callback = 0; const char *by = NULL; uint32_t by_len = 0;
+  if (argc >= 3) {
+    PkVarType type = pkGetSlotType(vm, 3);
+    if (type == PK_CLOSURE) {
+      callback = 3;
+      int arity;
+      if (!pkGetAttribute(vm, 3, "arity", 0) ||
+        pkGetSlotType(vm, 0) != PK_NUMBER ||
+        pkGetSlotNumber(vm, 0) != 1) {
+          pkSetRuntimeError(vm, "Expected exactly 1 argument for callback function.");
+          return;
+      }
+
+    } else if (type == PK_STRING) {
+      by = pkGetSlotString(vm, 3, &by_len);
+
+    } else {
+      pkSetRuntimeError(vm, "Expected a 'String' or a 'Closure' at slot 3.");
+      return;
+    }
+  }
 
   int32_t count = -1;
-  if (argc == 4) {
+  if (argc >= 4) {
     if (!pkValidateSlotInteger(vm, 4, &count)) return;
   }
 
-  const char *pattern, *input, *by;
-  uint32_t pattern_len, input_len, by_len;
-  capture* caps;
-  if (!_re_init(vm, &pattern, &input, &pattern_len, &input_len, &caps)) return;
-  if (!pkValidateSlotString(vm, 3, &by, &by_len)) return;
+  const char *input; uint32_t len;
+  RE* re = _re_init(vm, &input, &len, 5, NULL);
+  if (!re) return;
 
   pkByteBuffer buff;
   pkByteBufferInit(&buff);
 
-  int i = 0;
-  while (input_len > 0 && count != 0) {
-    i = slre_match(pattern, input, input_len, caps,
-      MAX_CAPTURES, 0);
+  const char *ptr = input;
+  while (ptr < input + len && count != 0) {
+    const char** matches = re_match(re, ptr);
+    if (!matches || !matches[0] || !matches[1] || matches[0] == matches[1]) break;
 
-    if (i < 0 || i > input_len) break;
-
-    if (caps[0].ptr - input != 0) {
-      pkByteBufferAddString(&buff, vm, input, caps[0].ptr - input);
+    if (matches[0] - ptr != 0) {
+      pkByteBufferAddString(&buff, vm, ptr, matches[0] - ptr);
     }
-    pkByteBufferAddString(&buff, vm, by, by_len);
-
-    input += i;
-    input_len -= i;
-
-    for(int j = 0; j < MAX_CAPTURES && caps[j].ptr != NULL; j++) {
-      caps[j].ptr = NULL; // reset the ptr after the string is stored
+    if (callback) {
+      pkSetSlotStringLength(vm, 0, matches[0], matches[1] - matches[0]);
+      pkCallFunction(vm, callback, 1, 0, 0);
+      if (pkGetSlotType(vm, 0) == PK_STRING) {
+        by = pkGetSlotString(vm, 0, &by_len);
+        pkByteBufferAddString(&buff, vm, by, by_len);
+      }
+    } else if (by) {
+      pkByteBufferAddString(&buff, vm, by, by_len);
     }
 
+    ptr = matches[1]; // point to last matched char
     if (count > 0) count--;
   }
-  if (input_len != 0) {
-    pkByteBufferAddString(&buff, vm, input, input_len);
+  if (ptr < input + len) {
+    pkByteBufferAddString(&buff, vm, ptr, input + len - ptr);
   }
 
   String* str = newStringLength(vm, (const char*)buff.data, buff.count);
   pkByteBufferClear(&buff, vm);
   vm->fiber->ret[0] = VAR_OBJ(str);
 
-  _re_error(vm, i);
-  _re_exit(vm, caps);
+  re_free(re);
 }
 
 DEF(_reSplit,
-  "re.split(pattern:String, input:String[, count:Number]) -> List",
+  "re.split(pattern:String, input:String[, count:Number, flag:Number]) -> List",
   "Split string by a regular expression.\n"
-  "Run help(re.match) for details."
+  "Run help(re.match) to show supported regex patterns."
   ) {
 
   int argc = pkGetArgc(vm);
-  if (!pkCheckArgcRange(vm, argc, 2, 3)) return;
+  if (!pkCheckArgcRange(vm, argc, 2, 4)) return;
 
   int32_t count = -1;
-  if (argc == 3) {
+  if (argc >= 3) {
     if (!pkValidateSlotInteger(vm, 3, &count)) return;
   }
 
-  const char *pattern, *input, *by;
-  uint32_t pattern_len, input_len, by_len;
-  capture* caps;
-  if (!_re_init(vm, &pattern, &input, &pattern_len, &input_len, &caps)) return;
+  const char *input; uint32_t len;
+  RE* re = _re_init(vm, &input, &len, 4, NULL);
+  if (!re) return;
 
-  pkReserveSlots(vm, 5);
   pkNewList(vm, 0);
 
-  int i = 0;
-  while (input_len > 0 && count != 0) {
-    i = slre_match(pattern, input, input_len, caps,
-      MAX_CAPTURES, 0);
+  const char *ptr = input;
+  while (ptr < input + len && count != 0) {
+    const char** matches = re_match(re, ptr);
+    if (!matches || !matches[0] || !matches[1] || matches[0] == matches[1]) break;
 
-    if (i < 0 || i > input_len) break;
+    pkSetSlotStringLength(vm, 1, ptr, matches[0] - ptr);
+    pkListInsert(vm, 0, -1, 1);
 
-    pkSetSlotStringLength(vm, 4, input, caps[0].ptr - input);
-    pkListInsert(vm, 0, -1, 4);
-
-    input += i;
-    input_len -= i;
-
-    for(int j = 0; j < MAX_CAPTURES && caps[j].ptr != NULL; j++) {
-      caps[j].ptr = NULL; // reset the ptr after the string is stored
-    }
-
+    ptr = matches[1]; // point to last matched char
     if (count > 0) count--;
   }
-  pkSetSlotStringLength(vm, 4, input, input_len);
-  pkListInsert(vm, 0, -1, 4);
+  pkSetSlotStringLength(vm, 1, ptr, input + len - ptr);
+  pkListInsert(vm, 0, -1, 1);
 
-  _re_error(vm, i);
-  _re_exit(vm, caps);
+  re_free(re);
 }
 
 /*****************************************************************************/
@@ -284,6 +284,7 @@ void registerModuleRe(PKVM* vm) {
   pkSetAttribute(vm, 0, "GLOBAL", 1);
 
   REGISTER_FN(re, "match", _reMatch, -1);
+  REGISTER_FN(re, "range", _reRange, -1);
   REGISTER_FN(re, "test", _reTest, -1);
   REGISTER_FN(re, "replace", _reReplace, -1);
   REGISTER_FN(re, "split", _reSplit, -1);
